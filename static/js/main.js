9ab73ae265c8a77c8f2c8fbb9d25743542493cfe
Muhamad Farid Kharismawan <faridkharismawan@gmail.com>
2022-01-28 17:05:28 +0700

    update style.css

--- static/js/main.js
+++ static/js/main.js
@@ -1,18 +1,26 @@
-// SHOW MENU
-const showMenu = (toggleId, navbarId,bodyId) =>{
-    const toggle = document.getElementById(toggleId),
-    navbar = document.getElementById(navbarId),
-    bodypadding = document.getElementById(bodyId)
-
-    if(toggle && navbar){
-        toggle.addEventListener('click', ()=>{
-            // APARECER MENU
-            navbar.classList.toggle('hide')
-            // ROTATE TOGGLE
-            toggle.classList.toggle('rotate')
-            // PADDING BODY
-            bodypadding.classList.toggle('reducer')
+/*==================== SHOW NAVBAR ====================*/
+const showMenu = (headerToggle, navbarId) =>{
+    const toggleBtn = document.getElementById(headerToggle),
+    nav = document.getElementById(navbarId)
+    
+    // Validate that variables exist
+    if(headerToggle && navbarId){
+        toggleBtn.addEventListener('click', ()=>{
+            // We add the show-menu class to the div tag with the nav__menu class
+            nav.classList.toggle('show-menu')
+            // change icon
+            toggleBtn.classList.toggle('bx-x')
         })
     }
 }
-showMenu('nav-toggle','navbar','body')
+showMenu('header-toggle','navbar')
+
+/*==================== LINK ACTIVE ====================*/
+const linkColor = document.querySelectorAll('.nav__link')
+
+function colorLink(){
+    linkColor.forEach(l => l.classList.remove('active'))
+    this.classList.add('active')
+}
+
+linkColor.forEach(l => l.addEventListener('click', colorLink))
